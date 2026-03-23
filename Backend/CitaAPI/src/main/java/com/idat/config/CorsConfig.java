694dc5678a314e7e7aac2fd7fbdfff023b8e89f4
Maximo <maxiiimo.tz@gmail.com>
2026-03-23 18:52:46 -0500

    feat: Implementación para conexión a movil

--- Backend/CitaAPI/src/main/java/com/idat/config/CorsConfig.java
+++ Backend/CitaAPI/src/main/java/com/idat/config/CorsConfig.java
@@ -13,11 +13,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+            	registry.addMapping("/**")
+	                .allowedOriginPatterns("*")
+	                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+	                .allowedHeaders("*")
+	                .allowCredentials(true);
             }
         };
     }
